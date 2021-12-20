5c591604b2c0e363ff10f852340dfff960fb5b9e
Pablo Varela Acosta <pablovarelaac@hotmail.com>
2021-12-19 22:42:01 -0300

    changes in landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './components/Navbar/logoFeikIT.svg'
+
 
 import  NavbarStore  from './components/Navbar/Navbar.jsx';
 
@@ -11,17 +12,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming Soon
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          Test
-        </a>
       </header>
     </div>
   );
